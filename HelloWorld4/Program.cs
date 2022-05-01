0cbdb64ee58267dd71c2d61e14dd2666c2104205
ROXANA lazarescu <roxana_lazarescu13@yahoo.com>
2022-05-01 15:31:23 +0300

    Add project files.

---
+++ HelloWorld4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorld4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //afisare PozitivNegativO
+            //daca am -5, imi afiseaza "-"
+            //daca am 5, imi afiseaza"+"
+            //daca am 0, imi afiseaza "0"
+            int x = -5;
+            if (x > 0)
+            {
+                Console.WriteLine("+");
+            }
+            else if (x < 0)
+            {
+
+                Console.WriteLine("-");
+            }
+            else
+            {
+                Console.WriteLine(x = 0);
+            }
+        }
+    }
+}
+
+
